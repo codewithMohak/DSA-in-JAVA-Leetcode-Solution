535d414fcb16db30f2406a09b73d9aa9b5791df7
Mohak Agarwal <155367989+codewithMohak@users.noreply.github.com>
2024-11-18 23:55:35 +0530

    Add files via upload

---
+++ reverseVowels.java
@@ -0,0 +1,36 @@
+public class reverseVowels {
+    public static void main(String[] args) {
+        String str = "welcometoscaler";
+        reverseVowels(str);
+    }
+
+    public static void reverseVowels(String str) {
+        char[] s = str.toCharArray();
+        int i = 0;
+        int j = s.length - 1;
+
+        while (i < j) {
+            while (s[i] != 'a' && s[i] != 'e' && s[i] != 'i' && s[i] != 'o' && s[i] != 'u') {
+                i++;
+            }
+            while (s[j] != 'a' && s[j] != 'e' && s[j] != 'i' && s[j] != 'o' && s[j] != 'u') {
+                j--;
+            }
+
+            if(i>=j){
+                break;
+            }
+
+            char ch = s[i];
+            s[i] = s[j];
+            s[j] = ch;
+
+            i++;
+            j--;
+        }
+
+        for(char ch:s){
+            System.out.print(ch);
+        }
+    }
+}
